2168ad9cc7f63f6f9c7b8aa5d53c2f170d218a05
Prateek <86514419+prateek-saxena-07@users.noreply.github.com>
2024-10-12 16:48:55 +0530

    commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = new express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173',credentials: true }));
+app.use(cors({ origin: '*',credentials: true }));
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/user', userRoute);
 app.use('/api/v1/temp', Video);
